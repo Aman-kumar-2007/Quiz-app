0133f4110677f68ee7ef18efb2827cb0c3075ab2
Aman Kumar <amanthecoder001@gmail.com>
2026-03-27 19:59:55 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// let currentQuestion = 0;
+// let totalQuestions = 10;
+
+// function updateProgress() {
+//   let percent = (currentQuestion / totalQuestions) * 100;
+  
+//   document.getElementById("progress").style.width = percent + "%";
+//   document.getElementById("progressText").innerText =
+//     currentQuestion + " / " + totalQuestions;
+// }
+
+// // call this when next question loads
+// function nextQuestion() {
+//   currentQuestion++;
+//   updateProgress();
+// }
