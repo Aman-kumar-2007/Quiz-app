0980053bada59dd08abfe892767670459160d638
Aman Kumar <amanthecoder001@gmail.com>
2026-03-27 23:17:42 +0530

    impliment js for menu open and close

--- app.js
+++ app.js
@@ -14,3 +14,19 @@
 //   currentQuestion++;
 //   updateProgress();
 // }
+
+const menubtn  = document.getElementById("menubtn");
+const menuList = document.getElementById("menuList");
+const aftermenubtn = document.getElementById("menubtn-after");
+const menuListAfter = document.getElementById("after-menu-hide");
+
+menubtn.addEventListener("click",()=>{
+    menuList.style.display ="none";
+    menuListAfter.style.display="block";
+})
+
+aftermenubtn.addEventListener("click",()=>{
+    menuListAfter.style.display="none";
+    menuList.style.display="flex";
+
+})
